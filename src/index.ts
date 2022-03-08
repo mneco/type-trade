5f31c60dbb5363573c4c9f2751134589f9ae22ad
constnw <constnw@gmail.com>
2022-03-09 00:10:11 +0100

    CI rerun

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-export function foo(a: number, b: number): number {
+export const foo = (a: number, b: number): number => {
 	return a + b;
-}
+};
 
-export function bar(a: number, b: number): number {
+export const bar = (a: number, b: number): number => {
 	return a - b;
-}
+};
